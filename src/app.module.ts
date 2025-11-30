1f3951e59ab20bba0d20db89c765b7bd3775d23c
rustytrooper <diansolo40@gmail.com>
2025-11-30 19:54:48 +0300

    feat: add required entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TracksModule } from './tracks/tracks.module';
+import { FavouritesModule } from './favourites/favourites.module';
+import { ArtistsModule } from './artists/artists.module';
+import { AlbumsModule } from './albums/albums.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, TracksModule, FavouritesModule, ArtistsModule, AlbumsModule],
   controllers: [AppController],
   providers: [AppService],
 })
